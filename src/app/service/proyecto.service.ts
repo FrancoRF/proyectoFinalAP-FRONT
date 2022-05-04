72281516b5306e98ebdf2e93d8e935d2dc75be5f
FrancoRF <francoAP2022@gmail.com>
2022-05-04 14:18:25 -0300

    Se completan los servicios

--- src/app/service/proyecto.service.ts
+++ src/app/service/proyecto.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectoService {
 
-  constructor() { }
+  apiBackPro = environment.apiBackPro;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getProyecto(): Observable<Proyecto[]> {
+    return this.http.get<Proyecto[]>(this.apiBackPro)
+  }
+
+  get(id: number): Observable<Proyecto> {
+    const url = `${this.apiBackPro}/${id}`
+    return this.http.get<Proyecto>(url);
+  }
+
+  masProyectos(formData: FormData): Observable<any> {
+    return this.http.post(this.apiBackPro + 'crear', formData);
+  }
+
+  editProyecto(id: number, formData: FormData): Observable<any> {
+    return this.http.put(this.apiBackPro + `editar/${id}`, formData)
+  }
+
+  eliminarProyecto(id: number): Observable<Proyecto> {
+    return this.http.delete<Proyecto>(this.apiBackPro + `eliminar/${id}`)
+  }
+
+
 }
--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -9,6 +9,21 @@ export class UiService {
   private agregarEspacio: boolean = false;
   private sujetoEspa = new Subject<any>();
 
+  private agregarExperiencia: boolean = false;
+  private sujetoExpe = new Subject<any>();
+
+  private agregarEducacion: boolean = false;
+  private sujetoEdu = new Subject<any>();
+
+  private agregarProyecto: boolean = false;
+  private sujetoPro = new Subject<any>();
+
+  private agregarHabilidad: boolean = false;
+  private sujetoHabi = new Subject<any>();
+
+  private agregarPerfil: boolean = false;
+  private sujetoPer = new Subject<any>();
+
   constructor() { }
 
   toggleAgregarEspacio():void {
@@ -19,5 +34,51 @@ export class UiService {
   onToggleEspacio(): Observable<any> {
     return this.sujetoEspa.asObservable();
   }
+
+  toggleAgregarExperiencia():void {
+    this.agregarExperiencia = !this.agregarExperiencia;
+    this.sujetoExpe.next(this.agregarExperiencia);
+  }
+
+  onToggleExperiencia(): Observable<any> {
+    return this.sujetoExpe.asObservable();
+  }
+
+  toggleAgregarEducacion():void {
+    this.agregarEducacion = !this.agregarEducacion;
+    this.sujetoEdu.next(this.agregarEducacion);
+  }
+
+  onToggleEducacion(): Observable<any> {
+    return this.sujetoEdu.asObservable();
+  }
+
+  toggleAgregarProyecto():void {
+    this.agregarProyecto = !this.agregarProyecto;
+    this.sujetoPro.next(this.agregarProyecto);
+  }
+
+  onToggleProyecto(): Observable<any> {
+    return this.sujetoPro.asObservable();
+  }
+
+  toggleAgregarHabilidad():void {
+    this.agregarHabilidad = !this.agregarHabilidad;
+    this.sujetoHabi.next(this.agregarHabilidad);
+  }
+
+  onToggleHabilidad(): Observable<any> {
+    return this.sujetoHabi.asObservable();
+  }
+
+  toggleAgregarPerfil():void {
+    this.agregarPerfil = !this.agregarPerfil;
+    this.sujetoPer.next(this.agregarPerfil);
+  }
+
+  onTogglePerfil(): Observable<any> {
+    return this.sujetoPer.asObservable();
+  }
+
 
 }

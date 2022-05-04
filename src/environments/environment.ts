041978d69ea126e74e67a7eb3bb61aab714bbbf3
FrancoRF <francoAP2022@gmail.com>
2022-05-04 14:10:35 -0300

    Se crean los servicios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
   apiBackAcer: 'http://localhost:8080/contenedores/',
+  apiBackExpe: 'http://localhost:8080/experiencias/',
+  apiBackEdu: 'http://localhost:8080/educaciones/',
+  apiBackPro: 'http://localhost:8080/proyectos/',
+  apiBackHabi: 'http://localhost:8080/habilidades/',
+  apiBackPer: 'http://localhost:8080/perfiles/'
 };

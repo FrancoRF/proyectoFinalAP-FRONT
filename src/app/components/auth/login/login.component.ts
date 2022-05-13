ca4ad806f690da152d16e2bb090d29532b373aaa
FrancoRF <francoAP2022@gmail.com>
2022-05-13 15:16:02 -0300

    Completo el Login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { Login } from 'src/app/models/login';
+import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +14,36 @@ export class LoginComponent implements OnInit {
 
   faUser = faUser;
   loginFail = false;
+  loginUsuario!: Login;
 
   nombreUsuario: string = "";
   password: string = "";
+  errMsj: string = "";
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    if(this.tokenService.traerToken()) {
+      this.loginFail = false;
+    }
   }
 
-  onLogin(){
+  onLogin(): void {
+    this.loginUsuario = new Login(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe({
+      next:(data) => {
+        this.loginFail = false;
+        this.tokenService.cambiarToken(data.token);
+        location.reload();
+      },
+      error:(err: any) => {
+        this.loginFail = true;
+        this.errMsj = err.error.message;
+      }
+    })
 
   }
 

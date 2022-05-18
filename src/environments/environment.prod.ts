be7bd21390ec4089a60b851562ba3c585ec09e67
FrancoRF <francoAP2022@gmail.com>
2022-05-18 20:37:44 -0300

    Creamos carpeta dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBackAcer: 'http://localhost:8080/auth/contenedores/',
+  apiBackExpe: 'http://localhost:8080/auth/experiencias/',
+  apiBackEdu: 'http://localhost:8080/auth/educaciones/',
+  apiBackPro: 'http://localhost:8080/auth/proyectos/',
+  apiBackHabi: 'http://localhost:8080/auth/habilidades/',
+  apiBackPer: 'http://localhost:8080/auth/perfiles/',
+  authUrl: 'http://localhost:8080/auth/'
 };
